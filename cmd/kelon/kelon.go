2d0aaee3f674be466ff34d19f470a849cba6d911
dcseifert <daniel.seifert@foundato.com>
2019-09-17 18:36:37 +0200

    Added basic REST proxy

--- cmd/kelon/kelon.go
+++ cmd/kelon/kelon.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/Foundato/kelon/configs"
 	"github.com/Foundato/kelon/internal/pkg/watcher"
+	"github.com/gorilla/mux"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
@@ -17,6 +23,8 @@ var (
 	// Flags
 	datastorePath = app.Flag("datastore-conf", "Path to the datastore configuration yaml.").Short('d').Default("./datastore.yml").Envar("DATASTORE_CONF").ExistingFile()
 	apiPath       = app.Flag("api-conf", "Path to the api configuration yaml.").Short('a').Default("./api.yml").Envar("API_CONF").ExistingFile()
+	proxyUrl      = app.Flag("proxy-url", "URL which is used to proxy OPA's Data-Api.").Default("/v1/data/authz/allow").Envar("PROXY_URL").URL()
+	port          = app.Flag("port", "Port on which the proxy endpoint is served.").Short('p').Default("8181").Envar("PORT").Int32()
 )
 
 type AppConfig struct {
@@ -24,6 +32,12 @@ type AppConfig struct {
 }
 
 var appConf = AppConfig{}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if _, err := fmt.Fprintf(w, "Hi there, kelon responding to %s!", r.URL.Path[1:]); err != nil {
+		log.Fatal("Unable to respond to HTTP request")
+	}
+}
 
 func main() {
 	app.HelpFlag.Short('h')
@@ -54,5 +68,44 @@ func onConfigLoaded(config *configs.Config, err error) {
 		log.Fatalln("Unable to parse configuration: ", err.Error())
 	}
 
-	fmt.Printf("%+v\n", config)
+	// Create Server and Route Handlers
+	log.Printf("Serving OPA's Data-Api at: http://localhost:%d%s\n", *port, (*proxyUrl).String())
+	r := mux.NewRouter()
+	r.HandleFunc((*proxyUrl).String(), handler)
+
+	server := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf(":%d", *port),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start Server
+	go func() {
+		log.Println("Starting Server")
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for shutdown
+	gracefulShutdown(server)
+}
+
+func gracefulShutdown(server *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalln("Error while shutting down server: ", err.Error())
+	}
+
+	log.Println("Shutting down...")
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190910110746-680d30ca3117 // indirect
 	github.com/google/go-cmp v0.3.1
+	github.com/gorilla/mux v1.7.3
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/yaml.v3 v3.0.0-20190905181640-827449938966
 )
