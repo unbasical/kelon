e38f1bcec4fc94e9b92f698faff0ce19f1e574fd
dcseifert <daniel.seifert@foundato.com>
2019-10-15 07:22:09 +0200

    Documented watcher package

--- internal/pkg/watcher/watcher.go
+++ internal/pkg/watcher/watcher.go
@@ -1,13 +1,26 @@
+// Package watcher contains components that are used for configuration reloading of kelon.
 package watcher
 
 import (
 	"github.com/Foundato/kelon/configs"
 )
 
+// ConfigWatcher is the interface that manages configuration reloading.
+//
+// Therefore a callback procedure is provided, which is always called if any configuration changes.
+type ConfigWatcher interface {
+
+	// Watches for configuration changes and calls the passed callback procedure every
+	// time the config changes.
+	Watch(callback func(*configs.ExternalConfig, error))
+}
+
+// New config watcher which only loads the config once on startup
 type DefaultConfigWatcher struct {
 	Loader configs.ConfigLoader
 }
 
+// See watcher.ConfigWatcher
 func (w DefaultConfigWatcher) Watch(callback func(*configs.ExternalConfig, error)) {
 	callback(w.Loader.Load())
 }
