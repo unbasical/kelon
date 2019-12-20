5073ba6c0463e2f5dc7da9157f72b59fb81d91c9
dcseifert <daniel.seifert@foundato.com>
2019-12-20 10:42:20 +0100

    Updated mongo example

--- examples/docker-compose/init/Init-Mongo.js
+++ examples/docker-compose/init/Init-Mongo.js
@@ -11,7 +11,7 @@ db.createUser(
     }
 );
 
-db.apps.insertOne({id:2,name:"Arnold's App",stars:3,rights:{right:"OWNER",user:{id:1,name:"Arnold",age:72,friend:"John Connor"}}});
+db.apps.insertOne({id:2,name:"Arnold's App",stars:3,rights:[{right:"OWNER",user:{id:1,name:"Arnold",age:72,friend:"John Connor"}}]});
 db.apps.insertOne({id:1,name:"First App for everyone",stars:1});
 db.apps.insertOne({id:3,name:"Famous App",stars:5});
 
