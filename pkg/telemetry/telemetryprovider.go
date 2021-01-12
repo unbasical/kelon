1880ef4fa57ea1d59f7513d40aa2bf33a37db9d3
andreasobermair <andreas.obermair@foundato.com>
2021-01-12 13:06:08 +0100

    Remove unused interface function

--- pkg/telemetry/telemetryprovider.go
+++ pkg/telemetry/telemetryprovider.go
@@ -11,8 +11,6 @@ type Provider interface {
 	GetHTTPMiddleware() (func(handler http.Handler) http.Handler, error)
 	// Get a handler which can be exposed as "/metrics" endpoint
 	GetHTTPMetricsHandler() (http.Handler, error)
-	// Check errors for additional metrics
-	CheckError(err error)
 	// Gracefully shutdown
 	Shutdown()
 }
